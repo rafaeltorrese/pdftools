0013f8c16efd724b4d16c4eb1a1229e52400e5d0
Rafael Torres Escobar <torresc.rafael@gmail.com>
2025-06-05 11:43:57 -0600

    os package to deal with path files

--- pdftext.py
+++ pdftext.py
@@ -1,11 +1,10 @@
+import os
 import textract
 from data import pathtofile, chapter_names
 
 
-
-
 for chapter in chapter_names:
-    path_file = f'{pathtofile}/{chapter}.pdf'
+    path_file = os.path.join(*pathtofile, f'{chapter}.pdf')
     text_bytes = textract.process(path_file)
     text = text_bytes.decode('utf-8')
     with open(f'{chapter}.txt', mode='w', encoding='utf-8', newline='') as file:
