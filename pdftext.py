8836ff4707b8b5cbf8a6c578a382b54732aa0697
Rafael Torres Escobar <torresc.rafael@gmail.com>
2025-07-22 14:15:14 -0600

    process all pdf files in batch

--- pdftext.py
+++ pdftext.py
@@ -1,11 +1,14 @@
 import os
 import textract
-from data import pathtofile, chapter_names
+from data import pathtofile
 
+file_list = os.listdir(os.path.join(*pathtofile))
 
-for chapter in chapter_names:
-    path_file = os.path.join(*pathtofile, f'{chapter}.pdf')
-    text_bytes = textract.process(path_file)
-    text = text_bytes.decode('utf-8')
-    with open(f'{chapter}.txt', mode='w', encoding='utf-8', newline='') as file:
-        file.write(text)
+for file in file_list:
+    base_file, ext_file = os.path.splitext(file)
+    if ext_file == '.pdf':
+        path_file = os.path.join(*pathtofile, file)
+        text_bytes = textract.process(path_file)
+        text = text_bytes.decode('utf-8')
+        with open(f'{base_file}.txt', mode='w', encoding='utf-8', newline='') as file:
+            file.write(text)
